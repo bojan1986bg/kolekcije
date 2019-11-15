31378d25c27fcf7f20c1b9f380a1538a4edd9ea7
okkursevi10 <okkursevi10@UC5PC6>
2019-11-15 21:54:56 +0100

    nizovi

---
+++ Kolekcije/src/nizovi/GlavnaKlasa.java
@@ -0,0 +1,63 @@
+package nizovi;
+
+public class GlavnaKlasa {
+
+	public static void main(String[] args) {
+	
+		int[]array = new int[5];
+		
+		// 1.nacin
+		
+		array[0] = 53;
+		array[1] = 29;
+		array[2] = 13;
+		array[3] = 62;
+		array[4] = 69;
+		
+		
+		System.out.println("Treci clan niza je " + array[2]);
+		
+		// 2. nacin
+		
+		int [] niz = {12,89,36,6,79	};
+		
+		System.out.println("Treci clan niza je " + niz[2]);
+		
+		// niz stringova
+		
+		String [] imena = {"Pera", "Vidoje", "Violeta", "Ljubica", "Marko"};
+		
+		System.out.println("Ovde ima " + imena.length + " imena");
+		
+		
+		//ispis uz pomoc for petlje
+		
+		for(int i = 0; i< imena.length; i++) {
+			
+			System.out.println(imena[i]);
+			
+			
+		}
+		
+		
+		//ispis uz while petlje
+		
+		int i = 0;
+		while(i< imena.length) {
+			System.out.println(imena[i]);
+		    i++;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
